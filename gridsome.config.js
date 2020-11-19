e7c1060e9b4dfb6dc41e43d3a1a0b9e5ecce2fd7
Alexander Sutcliffe <sutcliffe.alex@gmail.com>
2020-11-19 11:43:26 +0100

    change site name

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,7 +5,7 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
+  siteName: 'Gridsome Chart.js Examples',
   plugins: [
     {
       use: '@gridsome/vue-remark',
