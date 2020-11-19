e82dd9f370dae21f548d38b41bebf5d0ca3d8924
Alexander Sutcliffe <sutcliffe.alex@gmail.com>
2020-11-19 22:44:38 +0100

    bit of content

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,11 +1,13 @@
 <template>
   <Layout>
-
     <div class="container px-32 py-16">
       <div class="text-3xl font-bold py-8">Gridsome Chart.js Examples</div>
-    
 
-    <div class="py-8">Examples of basic usage of Chart.js in Gridsome using vue-chartjs.</div>
+      <div class="py-8">
+        Examples of basic usage of Chart.js in Gridsome using vue-chartjs. Uses
+        <code>chart.js</code> and <code>vue-chart-js</code> as well as
+        <code>vue-remark</code> for using components in markdown files.
+      </div>
     </div>
   </Layout>
 </template>
