5477c3c7cf770b4253c197efe5897161f466022f
Alexander Sutcliffe <sutcliffe.alex@gmail.com>
2020-11-19 11:44:21 +0100

    register components

--- src/main.js
+++ src/main.js
@@ -2,8 +2,13 @@
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
 import DefaultLayout from '~/layouts/Default.vue'
+import LineChart from '~/components/Line.vue';
+import BarChart from '~/components/Bar.vue';
 
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
+  Vue.component("LineChart", LineChart)
+  Vue.component("BarChart", BarChart)
+
 }
